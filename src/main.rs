a200cccc6033f42928e6b4d0cc9be483e6ed22bb
Elaina Bialkowski <elainajosephine.bialkowski-peluso@agr.gc.ca>
2024-01-24 15:12:14 -0500

    Implemented windows stay awake.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,33 @@
 use autopilot;
 use rand::prelude::*;
+use std::sync::{
+    atomic::{AtomicBool, Ordering::Relaxed},
+    Arc,
+};
+use windows::Win32::System::Power::{
+    SetThreadExecutionState, ES_CONTINUOUS, ES_DISPLAY_REQUIRED, ES_SYSTEM_REQUIRED,
+};
+
+fn keep_thread_awake() {
+    let flags = ES_CONTINUOUS | ES_SYSTEM_REQUIRED | ES_DISPLAY_REQUIRED;
+
+    unsafe { SetThreadExecutionState(flags) };
+}
+
+fn let_thread_sleep() {
+    unsafe {
+        SetThreadExecutionState(Default::default());
+    }
+}
 
 fn main() {
-    let screen_size = autopilot::screen::size();
-    let mut rng = rand::thread_rng();
-    loop {
+    keep_thread_awake();
+
+    let term = Arc::new(AtomicBool::new(false));
+    signal_hook::flag::register(signal_hook::consts::SIGTERM, Arc::clone(&term)).unwrap();
+    while !term.load(Relaxed) {
+        let screen_size = autopilot::screen::size();
+        let mut rng = rand::thread_rng();
         let (x, y) = (
             rng.gen_range(0.0..screen_size.width),
             rng.gen_range(0.0..screen_size.height),
@@ -12,4 +35,6 @@ fn main() {
         autopilot::mouse::smooth_move(autopilot::geometry::Point::new(x, y), Some(2.0)).unwrap();
         std::thread::sleep(std::time::Duration::from_secs(1));
     }
+
+    let_thread_sleep()
 }
